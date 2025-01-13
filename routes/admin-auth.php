5384464a727ce648470a3e4da98ab79dcdac9596
KarlaSolano <36199935+KarlaSolano@users.noreply.github.com>
2025-01-12 20:33:44 -0600

    modificación de rutas para administrador

--- routes/admin-auth.php
+++ routes/admin-auth.php
@@ -7,7 +7,7 @@ use App\Http\Controllers\Admin\Auth\PasswordResetLinkController;
 use App\Http\Controllers\Admin\ProfileController;
 use Illuminate\Support\Facades\Route;
 
-Route::middleware('guest:admin')->group(function () {
+Route::middleware('guest:admin')->prefix('admin')->name('admin.')->group(function () {
     Route::get('register', [RegisteredUserController::class, 'create'])
         ->name('register');
 
